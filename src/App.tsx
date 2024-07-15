b08c2543589c586468b117f3ef79122f6bea4bdb
Daniel Españadero <developer@daniel-espanadero.com>
2024-07-15 23:57:24 +0200

    Added sound effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
+// App.tsx
+import { useEffect } from "react";
+import useSound from "use-sound";
+import clickSound from "/sounds/public_sounds_click.mp3";
 import './App.css'
 
 function App() {
-  return (
-    <>
+  const [play] = useSound(clickSound, { volume: 0.05 });
 
-    </>
-  )
+  const handleClick = () => {
+    play();
+  };
+
+  useEffect(() => {
+    const resumeAudioContext = () => {
+      const AudioContext =
+        window.AudioContext || (window as any).webkitAudioContext;
+      if (AudioContext) {
+        const audioContext = new AudioContext();
+        if (audioContext.state === "suspended") {
+          audioContext
+            .resume()
+            .then(() => {
+              console.log("AudioContext resumed successfully");
+            })
+            .catch((error) => {
+              console.error("Error resuming AudioContext:", error);
+            });
+        }
+      }
+    };
+
+    window.addEventListener("click", resumeAudioContext);
+
+    return () => {
+      window.removeEventListener("click", resumeAudioContext);
+    };
+  }, []);
+
+  return (
+    <div className="App" onMouseDown={handleClick} onMouseUp={handleClick}>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
