b0d50ed9b3f44344a24d77339fb8dc4fe59c6b5d
Daniel Españadero <developer@daniel-espanadero.com>
2024-07-27 03:04:41 +0200

    Added book and course sections

--- src/pages/courses/Courses.tsx
+++ src/pages/courses/Courses.tsx
@@ -1,7 +1,33 @@
-import { CoursesStyled } from './CoursesStyled'
+import { CoursesStyled } from './CoursesStyled';
+import courses from '../../jsons/courses.json';
 
 export const Courses = () => {
   return (
-    <CoursesStyled>Courses</CoursesStyled>
-  )
+    <CoursesStyled>
+      <h1>CURSOS</h1>
+      <p className='courses-text'>
+        Aquí puedes encontrar una selección de mis cursos gratuitos que he
+        desarrollado a lo largo de mi carrera profesional. En ellos, no solo
+        demuestro mis conocimientos técnicos de manera práctica, sino que
+        también han acumulado más de 800,000 visualizaciones en total, validando
+        así la utilidad de dichos conocimientos por parte de los usuarios. En la actualidad sólo dispongo de cursos en español, aunque me gustaría subir material en inglés en un futuro.
+      </p>
+      <div className="courses-container">
+        {courses.map((course: any) => (
+          <a className="url-card" href={course.url} target='_BLANK'>
+            <div className="card">
+              <div className="img-container">
+                <img src={course.img} alt={course.title} />
+              </div>
+              <div className="text-card">
+                <p className="heading">{course.title}</p>
+                <p>{course.time}</p>
+                <p>{course.platform}</p>
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
+    </CoursesStyled>
+  );
 }
--- src/pages/courses/CoursesStyled.tsx
+++ src/pages/courses/CoursesStyled.tsx
@@ -1,5 +1,110 @@
 import styled from "styled-components";
 
 export const CoursesStyled = styled.div`
+  min-height: 100vh;
+  color: #fff;
+  h1 {
+    padding-top: 8rem;
+    text-align: center;
+  }
+  .courses-text {
+    display: inline-block;
+    width: 80%;
+    margin-left: 10%;
+    margin-top: 2rem;
+    font-size: 1.2rem;
+  }
+  .courses-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 100%;
+    min-height: 100vh;
+    margin: 4rem 0;
+  }
+  .url-card {
+    width: 228px;
+    height: 304px;
+    margin: 4rem;
+    .card {
+      position: relative;
+      width: 228px;
+      height: 304px;
+      background-color: #000;
+      display: flex;
+      flex-direction: column;
+      padding: 12px;
+      gap: 12px;
+      border-radius: 8px;
+      cursor: pointer;
+      .img-container {
+        position: relative;
+        top: 0;
+        width: 100%;
+        img {
+          width: 100%;
+        }
+      }
+      .text-card {
+        display: flex;
+        align-items: flex-start;
+        justify-content: space-evenly;
+        flex-direction: column;
+        width: 100%;
+        height: 60%;
+      }
+    }
 
-`
+    .card::before {
+      content: "";
+      position: absolute;
+      inset: 0;
+      left: -6px;
+      margin: auto;
+      width: 240px;
+      height: 317px;
+      border-radius: 10px;
+      background: linear-gradient(-45deg, #e81cff 0%, #40c9ff 100%);
+      z-index: -10;
+      pointer-events: none;
+      transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    }
+  }
+
+  .card::after {
+    content: "";
+    z-index: -1;
+    position: absolute;
+    inset: 0;
+    background: linear-gradient(-45deg, #fc00ff 0%, #00dbde 100%);
+    transform: translate3d(0, 0, 0) scale(0.95);
+    filter: blur(20px);
+  }
+
+  .heading {
+    font-size: 18px;
+    text-transform: capitalize;
+    font-weight: 700;
+    color: #fff;
+  }
+
+  .card p:not(.heading) {
+    font-size: 14px;
+    color: #fff;
+  }
+
+  .card p:last-child {
+    color: #e81cff;
+    font-weight: 600;
+  }
+
+  .card:hover::after {
+    filter: blur(30px);
+  }
+
+  .card:hover::before {
+    transform: rotate(-90deg) scaleX(1.34) scaleY(0.77);
+  }
+`;
